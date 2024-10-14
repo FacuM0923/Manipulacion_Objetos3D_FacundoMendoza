34012b627a1eec8e84fdc9a71a318c71d26682f3
FacuM0923 <facumendoza879@gmail.com>
2024-10-14 17:21:22 -0300

    Nuevo commit

--- TDPV_MO3D_TP01_Mendoza/Assets/Scripts/CubeBehaviour.cs
+++ TDPV_MO3D_TP01_Mendoza/Assets/Scripts/CubeBehaviour.cs
@@ -4,20 +4,11 @@ using UnityEngine;
 
 public class CubeBehaviour : MonoBehaviour {
 
-    public float speed = 5f;
-
-    public float rotationSpeed = 50f;
-
-    void Update(){
-
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        movementDirection.Normalize();
-
-        transform.position = transform.position + movementDirection * speed * Time.deltaTime;
-        if (movementDirection != Vector3.zero) transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movementDirection), rotationSpeed * Time.deltaTime);
+    void Awake(){
+        
     }
 
+    void Update(){
+        
+    }
 }
